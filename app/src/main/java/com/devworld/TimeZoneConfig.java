ac06e7a9cc0c1951fd630613446da4caa705a8ba
Shaidur Rahman <shaidur@metafour.com>
2026-01-23 18:50:48 +0600

    Intial timezone config

---
+++ app/src/main/java/com/devworld/TimeZoneConfig.java
@@ -0,0 +1,37 @@
+package com.devworld;
+
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class TimeZoneConfig {
+
+	@Value("${app.timezone:UTC}")
+	private String timezone;
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone(timezone));
+		log.info("Application timezone set to: {}", timezone);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		mapper.setTimeZone(TimeZone.getTimeZone(timezone));
+		return mapper;
+	}
+}
